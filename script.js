505c04a901fe8303de11228aa33196010a8dd0b9
Quinten Ross <qjross76@gmail.com>
2023-01-12 15:53:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var n = 0
 $(document).ready(function(){
+    
+$(".tvNavBar").click(function(){
+  $(".tvTable").scroll();
+    
+})
 
 $(".individualXboxAdvisory").click(function(){
   $("#xbox360Prices").toggle();
-    });                             
+    });           
+    
+    $(".individualPS2").click(function(){
+  $("#ps2Prices").toggle();
+    }); 
+    
    });                               
