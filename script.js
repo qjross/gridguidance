893c3174a3715f4accd490e4e5a7782f8463814c
Quinten Ross <qjross76@gmail.com>
2023-01-12 10:31:30 -0500

    Add files via upload
    Time for some jQuery

---
+++ script.js
@@ -0,0 +1,7 @@
+var n = 0
+$(document).ready(function(){
+
+$(".individualXboxAdvisory").click(function(){
+  $("#xbox360Prices").toggle();
+    });                             
+   });                               
