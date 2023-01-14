caa60442f27b96217fea236b38e7e4cd573678e1
Quinten Ross <qjross76@gmail.com>
2023-01-13 19:24:44 -0500

    sass version, re-added my commentary

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ $(".tvNavBar").click(function(){
     
 })
 
-$(".individualXboxAdvisory").click(function(){
+$(".individual360Advisory").click(function(){
   $("#xbox360Prices").toggle();
     });           
     
     $(".individualPS2").click(function(){
   $("#ps2Prices").toggle();
-    }); 
+    });
+    
+    $(".ddr1").click(function(){
+  $(".ddr1Price").toggle();
+    });       
+        $(".ddr2").click(function(){
+  $(".ddr2Price").toggle();
+    });    
+  
+        $(".ddr3").click(function(){
+  $(".ddr3Price").toggle();
+    });    
+        $(".ddr4").click(function(){
+  $(".ddr4Price").toggle();
+    });    
     
    });                               
