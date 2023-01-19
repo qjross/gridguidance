d6d789524b8c3d6b2a82f53a749934c32634f703
Quinten Ross <qjross76@gmail.com>
2023-01-18 20:50:34 -0500

    final commit for 1/18

--- script.js
+++ script.js
@@ -3,40 +3,61 @@ $(document).ready(function(){
     
 $(".individual360Advisory").click(function(){
   $("#xbox360Prices").toggle();
+    $("#ps2Prices").hide();
+    $("#ps3Prices").hide();
     });           
     
-    $(".individualPS2").click(function(){
+$(".individualPS2").click(function(){
   $("#ps2Prices").toggle();
+    $("#xbox360Prices").hide();
+    $("#ps3Prices").hide();
     });
     
-    $(".ddr1").click(function(){
+$(".individualPS3").click(function(){
+  $("#ps3Prices").toggle();
+    $("#ps2Prices").hide();
+    $("#xbox360Prices").hide();
+    });
+    
+$(".ddr1").click(function(){
   $(".ddr1Table").toggle();
-    });       
-        $(".ddr2").click(function(){
+    $(".ddr2Table").hide();
+    $(".ddr3Table").hide();
+    $(".ddr4Table").hide();
+    });
+    
+$(".ddr2").click(function(){
   $(".ddr2Table").toggle();
+    $(".ddr1Table").hide();
+    $(".ddr3Table").hide();
+    $(".ddr4Table").hide();
     });    
   
-        $(".ddr3").click(function(){
+$(".ddr3").click(function(){
   $(".ddr3Table").toggle();
+    $(".ddr1Table").hide();
+    $(".ddr2Table").hide();
+    $(".ddr4Table").hide();
     });    
-        $(".ddr4").click(function(){
+    
+$(".ddr4").click(function(){
   $(".ddr4Table").toggle();
+    $(".ddr1Table").hide();
+    $(".ddr2Table").hide();
+    $(".ddr3Table").hide();
     });    
     
-$(".navbar-brand").mouseover(function(){
- $(this).css({color: "#FFFFFF"});
- });
+$(".nav-item").mouseover(function() {
+   $(this).css({color:"#FFFFFF"});
     
- $(".navbar-brand").mouseout(function() {
-  $(this).removeAttr("style");
-}); 
-    
-$(".navbar").mouseover(function(){
-    $(this).css({opacity: 0.5});
 });
     
-$(".navbar").mouseleave(function(){
-    $(this).css({opacity: 1.0});
+$(".nav-item").mouseleave(function() {
+   $(this).removeAttr("style");                    
 });
+
+$(".x360HDD").click(function(){
+    $(".x360StorTable").toggle();
+        });
     
    });                               
